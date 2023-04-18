e77f649e6b8393acb9be60e4d4ac8a968af8abc2
David Onwuli <dcazicruz@icloud.com>
2023-04-18 00:58:24 +0000

    tried to correct task 7

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,4 @@
 export default function createReportObject(employeesList) {
-  let obj = {[`{allEmployees}`]: [...employeesList]};
+  const obj = {allEmployees: [...employeesList]};
   return obj
 }
