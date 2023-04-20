d652a343f5548fa299a668b850bbc0d17fa031d9
David Onwuli <dcazicruz@icloud.com>
2023-04-20 21:05:02 +0000

    added the solution for task0 of class task

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from ./0-classroom.js
+
+export default initializeRooms(){
+  lists[19,20,34];
+  value = [];
+  for(list of lists){
+    class1 = new Classroom(list)
+    value.append(class1._maxStudentsSize});
+  }
+  return [value];
+}
