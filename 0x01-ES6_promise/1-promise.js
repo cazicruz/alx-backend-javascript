263f4167bffbe8d7b9bf2e2dec8a131fdd05f2dc
David Onwuli <dcazicruz@icloud.com>
2023-04-20 07:39:34 +0000

    task0

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success){
-  const promise1 = new promise(true, false){
-    if (true){
-      return {status : 200, body :'success'};
+  const promise1 = new promise(function(resolve, reject){
+    if (success){
+      resolve({status : 200, body :'success'});
     }
-    else if(false){
-      return('The fake API is not working currently')
+    else{
+      reject(('The fake API is not working currently'));
     }
-  }
+  });
 }
