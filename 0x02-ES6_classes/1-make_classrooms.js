9de1fa66f26a5e53e64a6df19d77ec51acc2a1a9
David Onwuli <dcazicruz@icloud.com>
2023-04-21 10:48:59 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,5 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-  lists = [19,20,34];
-  value = [];
-  for(list of lists){
-    list = new ClassRoom(list)
-    value.push(list);
-  }
-  return [value];
+export default function initializeRooms() {
+  return [19, 20, 34].map((room) => new ClassRoom(room));
 }
