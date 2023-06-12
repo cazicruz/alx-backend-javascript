a78ba60ab5063ba42e3f3833cbd362203a28f770
david onwuli <dcazicruz@icloud.com>
2023-06-12 15:11:57 +0100

    added more files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,2 +1,16 @@
-let name = prompt("Welcome to Holberton School, what is your name?");
-alert(`Hello ${name}`);
+// Importing the 'readline' module for reading user input
+const readline = require('readline');
+
+// Creating a readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+// waiting for inpute
+rl.on('line', (input)=>{console.log(`Your name is:`, input)});
+
+// listen for closing
+rl.on('close', ()=>{console.log('This important software is now closing')});
