96261b86343e0e836744cf4527b1bdedfdadcb17
david onwuli <dcazicruz@icloud.com>
2023-06-13 20:58:56 +0100

    aded support for any endpoint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // 4-http.js
-const express = require('express');
+/** const express = require('express');
 const app = express();
 const port = 1245;
 
@@ -11,6 +11,17 @@ app.get('/*', (req, res) => {
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 }
-);
+);*/
+const http = require('http');
+app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Holberton School!');
+});
+
+const port = 1245;
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+});
+
 
 module.exports = app;
