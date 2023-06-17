e93871570fb45ec272b77e37d6fbe61a2f43b606
david onwuli <dcazicruz@icloud.com>
2023-06-17 13:20:10 +0100

    making change task

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul').calculateNumber;
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return the sum of rounded numbers', () => {
